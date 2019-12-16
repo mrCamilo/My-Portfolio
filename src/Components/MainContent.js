925d17be315423da6cb32111d174fbca0fd6441e
Manuel Camilo <mcamilo@protonmail.com>
2019-12-16 08:11:43 -0500

    added projects and contact page

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -2,47 +2,29 @@ import React from 'react'
 
 function MainContent() {
     return <main className="mainContainer">
-        <div className = "container" id="about">
-        <div className="section">
-            <h5>About Me</h5>
-            <p>My name is Manuel Camilo and I am a 23 year old developer from New Jersey. I graduated from Ramapo
-                College of NJ in 2019 in Economics and earned a certification from the Rutgers Coding Bootcamp in
-                December 2019.    
+        <div className="container" id="about">
+            <div className="section">
+                <h5>About Me</h5>
+                <p>My name is Manuel Camilo and I am a 23 year old developer from New Jersey. I graduated from Ramapo
+                    College of NJ in 2019 with a major in Economics and a minor in Literature then earned a
+                    certification from the Rutgers Coding Bootcamp in December 2019.
             </p>
-        </div>
-        <div className="divider" id="resume"></div>
-        <div className="section">
-            <h5>Resume</h5>
-            <p className="wordbreak">StuffStuffStuffStuffStuffS tuffS tuffStuffStuf fStuf fStuffStuffStu ffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuff StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffSt uffStuffStuffStuffStuffStuffStuf  tuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStu ffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffS tuffStuffStuffStuffS t uffStuff StuffStuffStuffStu fStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffSt uffStuffStuffStuffStu ffStuffStuffStuffStuffStuffStuff</p>
-        </div>
-        <div className="divider" id="projects"></div>
-        <div className="section">
-            <h5>Projects</h5>
-            <p className="wordbreak">StuffStuffStuffStuffStuffS tuffS tuffStuffStuf fStuf fStuffStuffStu ffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuff StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffSt uffStuffStuffStuffStuffStuffStuf  tuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStu ffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffS tuffStuffStuffStuffS t uffStuff StuffStuffStuffStu fStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffSt uffStuffStuffStuffStu ffStuffStuffStuffStuffStuffStuff</p>
-        </div>
-        <div className="divider" id="contact"></div>
-        <div className="section">
-            <h5>Contact Me!</h5>
-            <p className="wordbreak">StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff
-            StuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuffStuff</p>
-        </div>
+            </div>
+            <div className="divider" id="projects"></div>
+            <div className="section">
+                <h5>Projects</h5>
+                <ul>
+                    <li><a href="https://github.com/mrCamilo/Git-Clash">Git Clash</a></li>
+                    <li><a href="https://github.com/mrCamilo/Face-Fortune">Face Fortune</a></li>
+                </ul>
+            </div>
+            <div className="divider" id="contact"></div>
+            <div className="section">
+                <h5>Contact Me!</h5>
+                <p className="wordbreak">       
+                <a class="github-button" href="https://github.com/mrCamilo" aria-label="Follow @mrCamilo on GitHub">Follow @mrCamilo</a></p>
+                <p>Email: mcamilo@ramapo.edu</p>
+            </div>
         </div>
     </main>
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,6 @@ function Navbar() {
                         <ul className="right hide-on-med-and-down">
                             <li><a href="#home">Home</a></li>
                             <li><a href="#about">About Me</a></li>
-                            <li><a href="#resume">Resume</a></li>
                             <li><a href="#projects">Projects</a></li>
                             <li><a href="#contact">Contact Me</a></li>
                         </ul>
